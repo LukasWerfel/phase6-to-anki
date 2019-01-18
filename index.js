a455e94c32f1bfd7cb38153df9cff4709b80cbb2
Lukas Werfel <hi@lukaswerfel.com>
2019-01-18 18:00:34 +0100

    MVP

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-var striptags = require('striptags')
+const striptags = require('striptags')
+const stringify = require('csv-stringify')
+const { writeFileSync } = require('fs')
 
-const sourceFile = process.argv[2]
 
-const data = require(sourceFile)
-
-const cards = data.replyContent.cards
-
-const transformedCards = cards
+const extractRelevantData = data => data
   .map(({ cardContent, cardIdString }) => ({
     id: cardIdString,
     question: cardContent.question,
@@ -16,4 +13,20 @@ const transformedCards = cards
     role: striptags(cardContent.questionExample).split("\n")[0],
   }))
 
-console.log(transformedCards[0])
+const transformToTable = relevantData => {
+  const tableHeaders = [ "id", "foreign", "native", "label", "example", "role" ]
+  return [tableHeaders].concat(
+    relevantData.map(d => [d.id, d.question, d.answer, d.label, d.example, d.role])
+  )
+}
+
+const sourceFile = process.argv[2]
+const targetFile = process.argv[3]
+const sourceData = require(sourceFile).replyContent.cards
+
+const relevantData = extractRelevantData(sourceData)
+const table = transformToTable(relevantData)
+stringify(table, (err, csvString) => {
+  if (err) return console.error(err)
+  writeFileSync(targetFile, csvString, 'utf8')
+})
